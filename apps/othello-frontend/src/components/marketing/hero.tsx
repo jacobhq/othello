28f3b5c22158e92a3366110c3097e785f2270581
Jacob Marshall <me@jhqcat.com>
2025-11-04 16:58:53 +0000

    Move to pnpm, fly deployment

--- apps/othello-frontend/src/components/marketing/hero.tsx
+++ apps/othello-frontend/src/components/marketing/hero.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@/components/ui/button.tsx'
+import {Skeleton} from "@/components/ui/skeleton.tsx";
 
 export default function Hero() {
     return (
-        <section className="py-20">
+        <section className="py-16">
             <div className="relative z-10 mx-auto w-full max-w-2xl px-6 lg:px-0">
                 <div className="relative text-center">
                     <h1 className="mx-auto mt-16 max-w-xl text-balance text-5xl font-medium font-serif">Othello</h1>
@@ -35,14 +36,18 @@ export default function Hero() {
                         className="absolute inset-0 size-full object-cover"
                     />
 
-                    <div className="bg-background rounded-(--radius) relative m-4 overflow-hidden border border-transparent shadow-xl shadow-black/15 ring-1 ring-black/10 sm:m-8 md:m-12">
-                        <img
-                            src="/mist/tailark-2.png"
-                            alt="app screen"
-                            width="2880"
-                            height="1842"
-                            className="object-top-left size-full object-cover"
-                        />
+                    <div className="bg-white opacity-50 rounded-(--radius) relative m-4 overflow-hidden border border-transparent shadow-xl shadow-black/15 ring-1 ring-black/10 sm:m-8 md:m-12 aspect-square">
+                        <div className="grid grid-rows-8 rounded-md p-1 sm:p-2 xl:p-3 gap-1 sm:gap-2 xl:gap-3">
+                            {Array.from({length: 8}, (_, i) => (
+                                <div className="grid grid-cols-8 gap-1 sm:gap-2 xl:gap-3" key={i}>
+                                    {Array.from({length: 8}, (_, j) => (
+                                        <Skeleton className="bg-neutral-200 rounded-sm sm:rounded p-1 sm:p-2 xl:p3 aspect-square" key={j}>
+
+                                        </Skeleton>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 </div>
 
