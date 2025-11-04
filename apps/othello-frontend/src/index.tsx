3f837c020e896ce528b8bc856e50afc75c0ce0ba
Jacob Marshall <me@jhqcat.com>
2025-11-04 20:44:29 +0000

    Add analytics

--- apps/othello-frontend/src/index.tsx
+++ apps/othello-frontend/src/index.tsx
@@ -1,12 +1,20 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Toaster} from "@/components/ui/sonner"
+import {PostHogProvider} from "posthog-js/react";
 import './index.css'
 import App from './App.tsx'
 
+const options = {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    defaults: "2025-05-24",
+} as const
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App/>
-        <Toaster/>
+        <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+            <App/>
+            <Toaster/>
+        </PostHogProvider>
     </StrictMode>,
 )
