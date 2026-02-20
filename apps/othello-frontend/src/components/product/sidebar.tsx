d66135ca461ad4fb0c8eab9894f658a70dcf2e7c
Jacob Marshall <me@jhqcat.com>
2026-02-20 16:00:55 +0000

    Housekeeping, promote to beta

--- apps/othello-frontend/src/components/product/sidebar.tsx
+++ apps/othello-frontend/src/components/product/sidebar.tsx
@@ -1,8 +1,8 @@
-import type * as React from "react"
-import {PlayCircle, Circle, History, House} from "lucide-react"
+import type * as React from "react";
+import { PlayCircle, Circle, History, House } from "lucide-react";
 
-import {SidebarNavigation} from "@/components/product/sidebar-navigation"
-import {UserMenu} from "@/components/auth/user-menu"
+import { SidebarNavigation } from "@/components/product/sidebar-navigation";
+import { UserMenu } from "@/components/auth/user-menu";
 import {
   Sidebar as SidebarComponent,
   SidebarContent,
@@ -12,10 +12,10 @@ import {
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
-} from "@/components/ui/sidebar"
-import {Badge} from "@/components/ui/badge";
-import {Link, useLoaderData} from "@tanstack/react-router";
-import {Button} from "@/components/ui/button.tsx";
+} from "@/components/ui/sidebar";
+import { Badge } from "@/components/ui/badge";
+import { Link, useLoaderData } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button.tsx";
 
 const sidebarLinks = [
   {
@@ -32,11 +32,13 @@ const sidebarLinks = [
     title: "History",
     url: "/history",
     icon: History,
-  }
-]
+  },
+];
 
-export function Sidebar({...props}: React.ComponentProps<typeof SidebarComponent>) {
-  const user = useLoaderData({from: "/_dashboard"});
+export function Sidebar({
+  ...props
+}: React.ComponentProps<typeof SidebarComponent>) {
+  const user = useLoaderData({ from: "/_dashboard" });
 
   return (
     <SidebarComponent collapsible="offcanvas" {...props}>
@@ -45,13 +47,12 @@ export function Sidebar({...props}: React.ComponentProps<typeof SidebarComponent
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" tooltip="Othello" asChild>
               <Link to="/">
-                <div
-                  className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Circle className="size-4"/>
+                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+                  <Circle className="size-4" />
                 </div>
                 <div className="flex items-center gap-1.5 text-left text-sm leading-tight">
                   <span className="truncate font-semibold h-fit">Othello</span>
-                  <Badge variant="default">ALPHA</Badge>
+                  <Badge variant="default">Beta</Badge>
                 </div>
               </Link>
             </SidebarMenuButton>
@@ -59,13 +60,19 @@ export function Sidebar({...props}: React.ComponentProps<typeof SidebarComponent
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarNavigation items={sidebarLinks}/>
+        <SidebarNavigation items={sidebarLinks} />
       </SidebarContent>
       <SidebarFooter>
-        <Button variant="outline" id="sidebar-feedback" className="hidden md:flex">Give feedback</Button>
-        <UserMenu user={user}/>
+        <Button
+          variant="outline"
+          id="sidebar-feedback"
+          className="hidden md:flex"
+        >
+          Give feedback
+        </Button>
+        <UserMenu user={user} />
       </SidebarFooter>
-      <SidebarRail/>
+      <SidebarRail />
     </SidebarComponent>
-  )
+  );
 }
